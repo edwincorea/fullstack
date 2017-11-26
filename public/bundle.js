cf6d3c999f6c296106b1c0b8ad04bafff549425a
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-11-26 17:42:56 -0600

    Full SPA.

--- public/bundle.js
+++ public/bundle.js
@@ -47409,6 +47409,8 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactBootstrap = __webpack_require__(26);
 
+var _reactRouterBootstrap = __webpack_require__(410);
+
 var _reactRedux = __webpack_require__(30);
 
 var _redux = __webpack_require__(22);
@@ -47452,9 +47454,13 @@ var Menu = function (_React$Component) {
                         _reactBootstrap.Navbar.Brand,
                         null,
                         _react2.default.createElement(
-                            "a",
-                            { href: "/" },
-                            "Amazonian BooksShop"
+                            _reactRouterBootstrap.LinkContainer,
+                            { to: "/" },
+                            _react2.default.createElement(
+                                _reactBootstrap.NavItem,
+                                { eventKey: 0 },
+                                "Amazonian BooksShop"
+                            )
                         )
                     ),
                     _react2.default.createElement(_reactBootstrap.Navbar.Toggle, null)
@@ -47466,33 +47472,49 @@ var Menu = function (_React$Component) {
                         _reactBootstrap.Nav,
                         null,
                         _react2.default.createElement(
-                            _reactBootstrap.NavItem,
-                            { eventKey: 1, href: "/about" },
-                            "About"
+                            _reactRouterBootstrap.LinkContainer,
+                            { to: "/about" },
+                            _react2.default.createElement(
+                                _reactBootstrap.NavItem,
+                                { eventKey: 1 },
+                                "About"
+                            )
                         ),
                         _react2.default.createElement(
-                            _reactBootstrap.NavItem,
-                            { eventKey: 2, href: "/contact" },
-                            "Contact Us"
+                            _reactRouterBootstrap.LinkContainer,
+                            { to: "/contact" },
+                            _react2.default.createElement(
+                                _reactBootstrap.NavItem,
+                                { eventKey: 2 },
+                                "Contact Us"
+                            )
                         )
                     ),
                     _react2.default.createElement(
                         _reactBootstrap.Nav,
                         { pullRight: true },
                         _react2.default.createElement(
-                            _reactBootstrap.NavItem,
-                            { eventKey: 1, href: "/admin" },
-                            "Admin"
+                            _reactRouterBootstrap.LinkContainer,
+                            { to: "/admin" },
+                            _react2.default.createElement(
+                                _reactBootstrap.NavItem,
+                                { eventKey: 1 },
+                                "Admin"
+                            )
                         ),
                         _react2.default.createElement(
-                            _reactBootstrap.NavItem,
-                            { eventKey: 2, href: "/cart" },
-                            "Your Cart",
-                            totalQty > 0 ? _react2.default.createElement(
-                                _reactBootstrap.Badge,
-                                { className: "badge" },
-                                totalQty
-                            ) : ""
+                            _reactRouterBootstrap.LinkContainer,
+                            { to: "/cart" },
+                            _react2.default.createElement(
+                                _reactBootstrap.NavItem,
+                                { eventKey: 2 },
+                                "Your Cart",
+                                totalQty > 0 ? _react2.default.createElement(
+                                    _reactBootstrap.Badge,
+                                    { className: "badge" },
+                                    totalQty
+                                ) : ""
+                            )
                         )
                     )
                 )
@@ -47574,6 +47596,237 @@ var Footer = function (_React$Component) {
 }(_react2.default.Component);
 
 exports.default = Footer;
+
+/***/ }),
+/* 409 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+exports.__esModule = true;
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = __webpack_require__(4);
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _reactRouterDom = __webpack_require__(381);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var isModifiedEvent = function isModifiedEvent(event) {
+  return !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey);
+};
+
+var LinkContainer = function (_Component) {
+  _inherits(LinkContainer, _Component);
+
+  function LinkContainer() {
+    var _temp, _this, _ret;
+
+    _classCallCheck(this, LinkContainer);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, _Component.call.apply(_Component, [this].concat(args))), _this), _this.handleClick = function (event) {
+      var _this$props = _this.props,
+          children = _this$props.children,
+          onClick = _this$props.onClick;
+
+
+      if (children.props.onClick) {
+        children.props.onClick(event);
+      }
+
+      if (onClick) {
+        onClick(event);
+      }
+
+      if (!event.defaultPrevented && // onClick prevented default
+      event.button === 0 && // ignore right clicks
+      !isModifiedEvent(event) // ignore clicks with modifier keys
+      ) {
+          event.preventDefault();
+
+          var history = _this.context.router.history;
+          var _this$props2 = _this.props,
+              replace = _this$props2.replace,
+              to = _this$props2.to;
+
+
+          if (replace) {
+            history.replace(to);
+          } else {
+            history.push(to);
+          }
+        }
+    }, _temp), _possibleConstructorReturn(_this, _ret);
+  }
+
+  LinkContainer.prototype.render = function render() {
+    var _this2 = this;
+
+    var _props = this.props,
+        children = _props.children,
+        replace = _props.replace,
+        to = _props.to,
+        exact = _props.exact,
+        strict = _props.strict,
+        activeClassName = _props.activeClassName,
+        className = _props.className,
+        activeStyle = _props.activeStyle,
+        style = _props.style,
+        getIsActive = _props.isActive,
+        props = _objectWithoutProperties(_props, ['children', 'replace', 'to', 'exact', 'strict', 'activeClassName', 'className', 'activeStyle', 'style', 'isActive']);
+
+    var href = this.context.router.history.createHref(typeof to === 'string' ? { pathname: to } : to);
+
+    var child = _react2.default.Children.only(children);
+
+    return _react2.default.createElement(_reactRouterDom.Route, {
+      path: (typeof to === 'undefined' ? 'undefined' : _typeof(to)) === 'object' ? to.pathname : to,
+      exact: exact,
+      strict: strict,
+      children: function children(_ref) {
+        var location = _ref.location,
+            match = _ref.match;
+
+        var isActive = !!(getIsActive ? getIsActive(match, location) : match);
+
+        return _react2.default.cloneElement(child, _extends({}, props, {
+          className: [className, child.props.className, isActive ? activeClassName : null].join(' ').trim(),
+          style: isActive ? _extends({}, style, activeStyle) : style,
+          href: href,
+          onClick: _this2.handleClick
+        }));
+      }
+    });
+  };
+
+  return LinkContainer;
+}(_react.Component);
+
+LinkContainer.contextTypes = {
+  router: _propTypes2.default.shape({
+    history: _propTypes2.default.shape({
+      push: _propTypes2.default.func.isRequired,
+      replace: _propTypes2.default.func.isRequired,
+      createHref: _propTypes2.default.func.isRequired
+    }).isRequired
+  }).isRequired
+};
+LinkContainer.propTypes = {
+  children: _propTypes2.default.element.isRequired,
+  onClick: _propTypes2.default.func,
+  replace: _propTypes2.default.bool,
+  to: _propTypes2.default.oneOfType([_propTypes2.default.string, _propTypes2.default.object]).isRequired,
+  exact: _propTypes2.default.bool,
+  strict: _propTypes2.default.bool,
+  className: _propTypes2.default.string,
+  activeClassName: _propTypes2.default.string,
+  style: _propTypes2.default.object,
+  activeStyle: _propTypes2.default.object,
+  isActive: _propTypes2.default.func
+};
+LinkContainer.defaultProps = {
+  replace: false,
+  exact: false,
+  strict: false,
+  activeClassName: 'active'
+};
+exports.default = LinkContainer;
+module.exports = exports['default'];
+
+/***/ }),
+/* 410 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+exports.__esModule = true;
+exports.LinkContainer = exports.IndexLinkContainer = undefined;
+
+var _IndexLinkContainer2 = __webpack_require__(411);
+
+var _IndexLinkContainer3 = _interopRequireDefault(_IndexLinkContainer2);
+
+var _LinkContainer2 = __webpack_require__(409);
+
+var _LinkContainer3 = _interopRequireDefault(_LinkContainer2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.IndexLinkContainer = _IndexLinkContainer3.default;
+exports.LinkContainer = _LinkContainer3.default;
+
+/***/ }),
+/* 411 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+exports.__esModule = true;
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _LinkContainer = __webpack_require__(409);
+
+var _LinkContainer2 = _interopRequireDefault(_LinkContainer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// Don't use a stateless function, to allow users to set a ref.
+/* eslint-disable react/prefer-stateless-function */
+var IndexLinkContainer = function (_React$Component) {
+  _inherits(IndexLinkContainer, _React$Component);
+
+  function IndexLinkContainer() {
+    _classCallCheck(this, IndexLinkContainer);
+
+    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
+  }
+
+  IndexLinkContainer.prototype.render = function render() {
+    return _react2.default.createElement(_LinkContainer2.default, _extends({}, this.props, { exact: true }));
+  };
+
+  return IndexLinkContainer;
+}(_react2.default.Component);
+/* eslint-enable react/prefer-stateless-function */
+
+
+exports.default = IndexLinkContainer;
+module.exports = exports['default'];
 
 /***/ })
 /******/ ]);
--- src/client/components/menu.js
+++ src/client/components/menu.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {Nav, NavItem, Navbar, Badge} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+
 import { getCart } from "../../client/actions/cartActions";
 
 class Menu extends React.Component {
@@ -16,20 +18,30 @@ class Menu extends React.Component {
             <Navbar inverse fixedTop>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="/">Amazonian BooksShop</a>
+                        <LinkContainer to="/">
+                            <NavItem eventKey={0}>Amazonian BooksShop</NavItem>
+                        </LinkContainer>                                    
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav>
-                        <NavItem eventKey={1} href="/about">About</NavItem>
-                        <NavItem eventKey={2} href="/contact">Contact Us</NavItem>
+                        <LinkContainer to="/about">
+                            <NavItem eventKey={1}>About</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/contact">
+                            <NavItem eventKey={2} >Contact Us</NavItem>
+                        </LinkContainer>
                     </Nav>
                     <Nav pullRight>
-                        <NavItem eventKey={1} href="/admin">Admin</NavItem>
-                        <NavItem eventKey={2} href="/cart">Your Cart
-                            { totalQty > 0 ? <Badge className="badge">{totalQty}</Badge> : ""}
-                        </NavItem>
+                        <LinkContainer to="/admin">
+                            <NavItem eventKey={1}>Admin</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/cart">
+                            <NavItem eventKey={2}>Your Cart
+                                { totalQty > 0 ? <Badge className="badge">{totalQty}</Badge> : ""}
+                            </NavItem>                    
+                        </LinkContainer>                                                
                     </Nav>                    
                 </Navbar.Collapse>
             </Navbar>                                        
